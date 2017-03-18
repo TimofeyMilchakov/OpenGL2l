0889586d469af997efb103043a30816f001af718
TimofeyMilchakov <timofey.milchakov@mail.ru>
2017-03-18 22:39:24 +0300

    qw

---
+++ mylines.h
@@ -0,0 +1,21 @@
+#ifndef MYLINES_H
+#define MYLINES_H
+
+class myLines
+{
+public:
+    float x1;
+    float x2;
+    float y1;
+    float y2;
+    myLines(float _x1, float _y1, float _x2, float _y2);
+    myLines();
+    myLines getLine1();
+     myLines getLine2();
+     float getLong();
+     myLines getLine3();
+     myLines getLine4();
+};
+
+#endif // MYLINES_H
+
